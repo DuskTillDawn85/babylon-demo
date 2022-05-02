75d6a6cb6ad781edc605579d394faae2ba16cca8
shawn <1067608727@qq.com>
2022-05-02 15:30:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="canvas-wrap">
+    <canvas ref="canvas" id="renderCanvas"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import initGames from "./plugins/babylon";
+import { ref, onMounted } from "vue";
+
+const canvas = ref<HTMLCanvasElement>();
+onMounted(() => {
+  canvas.value && initGames(canvas.value);
+});
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.canvas-wrap {
+  border: 1px solid #2c3e50;
+  height: 600px;
+
+  #renderCanvas {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ src/plugins/babylon.ts
@@ -0,0 +1,87 @@
+import * as BABYLON from "babylonjs";
+import { Vector3 } from "babylonjs";
+import "babylonjs-loaders";
+import "./cannon.js";
+// import './ammo.js'
+
+enum State {
+  READY = 0,
+  PLAY = 1,
+  LOSE = 2,
+}
+
+const createScene = async (
+  engine: BABYLON.Engine,
+  canvas: HTMLCanvasElement
+): Promise<BABYLON.Scene> => {
+  const scene = new BABYLON.Scene(engine);
+
+  // 激活物理引擎
+  const physicsPlugin = new BABYLON.CannonJSPlugin();
+  scene.enablePhysics();
+
+  // 加载角色模型
+  const seagulf = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "src/assets/models/",
+    "seagulf.glb",
+    scene
+  );
+
+  // 调整角色大小、朝向和位置
+  seagulf.meshes[0].scaling.scaleInPlace(0.3);
+  seagulf.meshes[0].position = new BABYLON.Vector3(0, 152, 0);
+  seagulf.meshes[0].rotation = new BABYLON.Vector3(0, 4.8, 0);
+
+  // 创建自由视角相机
+  const camera = new BABYLON.ArcRotateCamera(
+    "camera",
+    0,
+    Math.PI / 3,
+    1000,
+    new BABYLON.Vector3(10, 10, 10),
+    scene
+  );
+
+  // 设置相机位置到场景初始位置
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, true);
+
+  // 添加环境光源
+  const light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0.8, 1, 0), scene);
+  // 降低亮度
+  light.intensity = 0.7;
+
+
+  // 创建地板砖
+  const faceColors = new Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    if (i === 4) faceColors[i] = new BABYLON.Color4(0.13, 0.68, 1, 0.8);
+    else faceColors[i] = new BABYLON.Color4(1,1,1,1);
+  }
+  const tiledBox = BABYLON.MeshBuilder.CreateBox(
+    "",
+    {
+      faceColors: faceColors,
+      size: 500,
+      height: 100,
+    },
+    scene
+  );
+  tiledBox.position = new BABYLON.Vector3(160,-50,0)
+
+  // 返回最终渲染场景
+  return scene;
+};
+
+export default async function initGames(canvas: HTMLCanvasElement) {
+  let engine = new BABYLON.Engine(canvas, true);
+
+  const scene = await createScene(engine, canvas);
+
+  engine.runRenderLoop(() => scene.render());
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+}
